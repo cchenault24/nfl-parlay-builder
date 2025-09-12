ecdf738f38fa464afce6678c9934174573c12255
Christian Chenault <cchenault24@yahoo.com>
2025-09-11 20:48:31 -0400

    lint fix

--- src/components/display/ParlayLanding.tsx
+++ src/components/display/ParlayLanding.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import { Card, CardContent, Typography } from '@mui/material'
+import React from 'react'
 
 const ParlayLanding: React.FC = () => (
   <Card>
     <CardContent sx={{ textAlign: 'center', py: 4 }}>
       <Typography variant="h6" color="text.secondary">
-        Select a game and click "Create 3-Leg Parlay" to get started
+        {'Select a game and click "Create 3-Leg Parlay" to get started'}
       </Typography>
     </CardContent>
   </Card>
